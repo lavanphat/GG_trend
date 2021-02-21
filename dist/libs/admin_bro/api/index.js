6791fa86b04a0f06a39e47024e3192c2d94634bd
Van Phat <vanphathutechit@gmail.com>
2021-02-21 19:49:14 +0700

    update link call api

--- dist/libs/admin_bro/api/index.js
+++ dist/libs/admin_bro/api/index.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.URL = void 0;
 exports.URL = process.env.NODE_ENV === 'dev'
     ? 'http://localhost:3000'
-    : 'https://gg-trend.glitch.me';
+    : 'https://gg-trends.herokuapp.com';
 //# sourceMappingURL=index.js.map
--- libs/admin_bro/api/index.ts
+++ libs/admin_bro/api/index.ts
@@ -1,4 +1,4 @@
 export const URL: string =
   process.env.NODE_ENV === 'dev'
     ? 'http://localhost:3000'
-    : 'https://gg-trend.glitch.me';
+    : 'https://gg-trends.herokuapp.com';
